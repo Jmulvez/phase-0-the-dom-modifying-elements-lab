3c2df699a652ca9f2f6906abf9245b5fd1cbc572
John <johnmulvey48@yahoo.com>
2021-12-28 11:57:48 -0500

    git@github.com:Jmulvez/phase-0-the-dom-modifying-elements-lab.git

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-// Write your code here!
+const ul = document.getElementsById("head");
+const secondChild = ul.querySelector("main");
+ul.removeChild(main);
+
+const element = document.getElementById("h1");
+element.replaceWith = "victory";
+
+const h1#victory = document.getElementById("main");
+h1.innerHTML =
+  "<h1>"John is the champion</h1>
+  
